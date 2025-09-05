c8013ce690e07a70f84cba9cead4ad032e9e81c1
Natis-san <62031815+LeBN@users.noreply.github.com>
2025-09-05 17:04:31 +0200

    Ex3 test

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const cube = document.getElementsByClassName("interactable");
+
+    cube.addEventListener("OnClick", (event) => {
+        if (cube.color == "red"){
+            cube.color = "blue";
+        } else {
+            cube.color = "red";
+        }
+    });
+
+});
